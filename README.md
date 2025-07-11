02607d65b829f548a27dabd8ecaad6b93519faa0
Metasebiya-21 <metasebiya8@gmail.com>
2025-07-11 12:13:26 +0300

    fixing issue of not finding the simple_arm_description

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # robotic_arm
+## Rebuild and source
+colcon build --packages-select simple_arm_description
+source install/setup.bash
+
+## verify urdf
+ros2 run xacro xacro   ~/ros2_ws/src/simple_arm_description/urdf/simple_arm.urdf.xacro   > /tmp/simple_arm.urdf
+
+## Test Launch
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+source setup_simple_arm.sh
+ros2 launch simple_arm_description sim_arm.launch.py
--- setup_simple_arm.sh
+++ setup_simple_arm.sh
@@ -1,3 +1,8 @@
 export GZ_VERSION=fortress
 export IGN_GAZEBO_RESOURCE_PATH=/home/metasebiya/ros2_ws/src/simple_arm_description/models
 export ROS_PLUGINLIB_PLUGIN_PATH=/opt/ros/humble/lib:$ROS_PLUGINLIB_PLUGIN_PATH
+# where your libign_ros2_control-system.so lives
+export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=/opt/ros/humble/lib:${IGN_GAZEBO_SYSTEM_PLUGIN_PATH:-}
+
+# also make sure the loader can pull in its dependencies
+export LD_LIBRARY_PATH=/opt/ros/humble/lib:${LD_LIBRARY_PATH:-}
